abac997b02a041dd47c86d46b2aa05a4d681af6d
Ryan <ryanbiy01@gmail.com>
2023-03-16 20:05:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    const newArray=[]
+    for(let i=0; i<names.length; i++){
+        const message=`Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+       newArray.push(message)
+
+    }
+    return newArray;
+}
+console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"));
+
+
+//countDown
+function countDown(){
+    let number=10;
+    while (number>=0){
+
+        console.log(number--)
+    }
+
+
+}
